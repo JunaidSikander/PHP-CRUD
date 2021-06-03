f9e9e492538903959158afa33cb7264b509c9bd9
Junaid Sikander Hakro <junaid_sikander@live.com>
2021-06-03 12:25:44 +0500

    [ADD] Home content

--- index.php
+++ index.php
@@ -1,2 +1,76 @@
 <?php
-    include 'components/header.php';
+include 'components/header.php';
+?>
+<div id="main-content">
+    <h2>All Records</h2>
+    <table cellpadding="7px">
+        <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Class</th>
+        <th>Phone</th>
+        <th>Action</th>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Junaid</td>
+            <td>Larkana</td>
+            <td>CS</td>
+            <td>9276543210</td>
+            <td>
+                <a href='components/edit.php'>Edit</a>
+                <a href='components/delete.php'>Delete</a>
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Moin uddin</td>
+            <td>Nawabshah</td>
+            <td>BCS</td>
+            <td>9276543210</td>
+            <td>
+                <a href='components/edit.php'>Edit</a>
+                <a href='components/delete.php'>Delete</a>
+            </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Aniss ur Rehman</td>
+            <td>Hyderabad</td>
+            <td>CE</td>
+            <td>9276543210</td>
+            <td>
+                <a href='components/edit.php'>Edit</a>
+                <a href='components/delete.php'>Delete</a>
+            </td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Abdul Sami</td>
+            <td>Karachi</td>
+            <td>EL</td>
+            <td>9276543210</td>
+            <td>
+                <a href='components/edit.php'>Edit</a>
+                <a href='components/delete.php'>Delete</a>
+            </td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Manzoor</td>
+            <td>Thal</td>
+            <td>ES</td>
+            <td>9276543210</td>
+            <td>
+                <a href='components/edit.php'>Edit</a>
+                <a href='components/delete.php'>Delete</a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+</body>
+</html>
